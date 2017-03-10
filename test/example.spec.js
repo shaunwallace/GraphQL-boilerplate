42687a90b3533b01e6c1e4b9e94666937193875d
Shaun Wallace <swallace@sapient.com>
2017-03-10 12:39:11 +0000

    adding coveralls, appveyor.yml

--- test/example.spec.js
+++ test/example.spec.js
@@ -1,5 +1,11 @@
+import base from '../src';
+
 describe('a suite', () => {
     it('runs a spec', () => {
         expect(true).toEqual(true);
+    });
+
+    it('should return true', () => {
+        expect(base()).toEqual(true);
     });
 });
